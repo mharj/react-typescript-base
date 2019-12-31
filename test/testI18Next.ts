0113ad382ad90d341a7082f5f3243aa1627c1c3e
Marko Harjula <marko.harjula@gmail.com>
2019-12-31 15:25:59 +0200

    version upgrades

--- test/testI18Next.ts
+++ test/testI18Next.ts
@@ -3,7 +3,7 @@ import {expect} from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import chaiSubset from 'chai-subset';
 import 'cross-fetch/polyfill';
-import {exists, t} from 'i18next';
+import i18next from 'i18next';
 import 'mocha';
 import '../src/i18n';
 
@@ -13,9 +13,9 @@ chai.use(chaiSubset);
 
 describe('test i18next instance', () => {
 	it('should not found transation key', async () => {
-		expect(exists('hello')).to.be.eq(true);
+		expect(i18next.exists('hello')).to.be.eq(true);
 	});
 	it('should translate helloe', async () => {
-		expect(t('hello')).to.be.eq('Hello');
+		expect(i18next.t('hello')).to.be.eq('Hello');
 	});
 });
